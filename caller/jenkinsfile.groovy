ceadfd7b54faddc29aec4f900e6cee3cc15520ec
root <root@ip-172-31-12-204.ec2.internal>
2018-10-24 17:21:45 +0000

    syntaxis correction

--- caller/jenkinsfile.groovy
+++ caller/jenkinsfile.groovy
@@ -9,7 +9,7 @@ try { //using try/catch structure to get the errors
 	println responder //prints the variable       
 	responder.hello('hi')
 	}catch (Exception err){     
-	println err   }  } }} 
+	println err   }  } } 
 
 def repoclone(){ //defines a function to clone the repositories to the Jenkins' workspace  
 checkout([$class: 'GitSCM', branches: [[name: '*/'+'master']], 
